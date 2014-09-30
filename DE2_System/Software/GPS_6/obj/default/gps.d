c2317a0b47f1cffe121aee0fe1dd787fb89f11dc
matthewlh <matthewlhandley@gmail.com>
2014-09-30 11:02:07 -0600

    Formatted statements on LCD correct.

---
+++ DE2_System/Software/GPS_6/obj/default/gps.d
@@ -0,0 +1,30 @@
+obj/default/gps.o: gps.c gps.h main.h ../GPS_6_bsp//HAL/inc/alt_types.h \
+  ../GPS_6_bsp/system.h ../GPS_6_bsp/linker.h \
+  ../GPS_6_bsp//HAL/inc/sys/alt_irq.h ../GPS_6_bsp//HAL/inc/nios2.h \
+  ../GPS_6_bsp//HAL/inc/alt_types.h ../GPS_6_bsp/system.h \
+  ../GPS_6_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  ../GPS_6_bsp//HAL/inc/io.h ../GPS_6_bsp//HAL/inc/alt_types.h
+
+gps.h:
+
+main.h:
+
+../GPS_6_bsp//HAL/inc/alt_types.h:
+
+../GPS_6_bsp/system.h:
+
+../GPS_6_bsp/linker.h:
+
+../GPS_6_bsp//HAL/inc/sys/alt_irq.h:
+
+../GPS_6_bsp//HAL/inc/nios2.h:
+
+../GPS_6_bsp//HAL/inc/alt_types.h:
+
+../GPS_6_bsp/system.h:
+
+../GPS_6_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../GPS_6_bsp//HAL/inc/io.h:
+
+../GPS_6_bsp//HAL/inc/alt_types.h:
