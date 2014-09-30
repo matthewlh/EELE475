8016d15d07ef0e73b54b5e50688f96cb2ba860ad
matthewlh <matthewlhandley@gmail.com>
2014-09-30 09:56:29 -0600

    It Works!
    The project can be loaded, now to test the LCD.

---
+++ DE2_System/Software/gps_5/obj/default/gps.d
@@ -0,0 +1,30 @@
+obj/default/gps.o: gps.c gps.h main.h ../gps_5_bsp//HAL/inc/alt_types.h \
+  ../gps_5_bsp/system.h ../gps_5_bsp/linker.h \
+  ../gps_5_bsp//HAL/inc/sys/alt_irq.h ../gps_5_bsp//HAL/inc/nios2.h \
+  ../gps_5_bsp//HAL/inc/alt_types.h ../gps_5_bsp/system.h \
+  ../gps_5_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  ../gps_5_bsp//HAL/inc/io.h ../gps_5_bsp//HAL/inc/alt_types.h
+
+gps.h:
+
+main.h:
+
+../gps_5_bsp//HAL/inc/alt_types.h:
+
+../gps_5_bsp/system.h:
+
+../gps_5_bsp/linker.h:
+
+../gps_5_bsp//HAL/inc/sys/alt_irq.h:
+
+../gps_5_bsp//HAL/inc/nios2.h:
+
+../gps_5_bsp//HAL/inc/alt_types.h:
+
+../gps_5_bsp/system.h:
+
+../gps_5_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../gps_5_bsp//HAL/inc/io.h:
+
+../gps_5_bsp//HAL/inc/alt_types.h:
