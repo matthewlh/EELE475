2ec118fcedfe319b819a6b6795eacd7720f7ddf8
davidkeltgen <dkeltgen45@gmail.com>
2014-10-28 11:21:11 -0600

    Multiple tasks created
    Getting gps characters task and writing strings to LCD tasks created and
    running simultaneously.

---
+++ DE2_System/Software/lab_08_2/obj/default/alt_ucosii_simple_error_check.d
@@ -0,0 +1,47 @@
+obj/default/alt_ucosii_simple_error_check.o:  \
+ alt_ucosii_simple_error_check.c ../lab_08_2_bsp//HAL/inc/includes.h \
+  ../lab_08_2_bsp//HAL/inc/os_cpu.h \
+  ../lab_08_2_bsp//HAL/inc/sys/alt_irq.h ../lab_08_2_bsp//HAL/inc/nios2.h \
+  ../lab_08_2_bsp//HAL/inc/alt_types.h ../lab_08_2_bsp/system.h \
+  ../lab_08_2_bsp/linker.h ../lab_08_2_bsp//UCOSII/inc/os_cfg.h \
+  ../lab_08_2_bsp//HAL/inc/sys/alt_alarm.h \
+  ../lab_08_2_bsp//HAL/inc/sys/alt_llist.h \
+  ../lab_08_2_bsp//HAL/inc/priv/alt_alarm.h \
+  ../lab_08_2_bsp//HAL/inc/alt_types.h ../lab_08_2_bsp/system.h \
+  ../lab_08_2_bsp//UCOSII/inc/ucos_ii.h \
+  ../lab_08_2_bsp//UCOSII/inc/os_cfg.h ../lab_08_2_bsp//HAL/inc/os_cpu.h \
+  alt_ucosii_simple_error_check.h
+
+../lab_08_2_bsp//HAL/inc/includes.h:
+
+../lab_08_2_bsp//HAL/inc/os_cpu.h:
+
+../lab_08_2_bsp//HAL/inc/sys/alt_irq.h:
+
+../lab_08_2_bsp//HAL/inc/nios2.h:
+
+../lab_08_2_bsp//HAL/inc/alt_types.h:
+
+../lab_08_2_bsp/system.h:
+
+../lab_08_2_bsp/linker.h:
+
+../lab_08_2_bsp//UCOSII/inc/os_cfg.h:
+
+../lab_08_2_bsp//HAL/inc/sys/alt_alarm.h:
+
+../lab_08_2_bsp//HAL/inc/sys/alt_llist.h:
+
+../lab_08_2_bsp//HAL/inc/priv/alt_alarm.h:
+
+../lab_08_2_bsp//HAL/inc/alt_types.h:
+
+../lab_08_2_bsp/system.h:
+
+../lab_08_2_bsp//UCOSII/inc/ucos_ii.h:
+
+../lab_08_2_bsp//UCOSII/inc/os_cfg.h:
+
+../lab_08_2_bsp//HAL/inc/os_cpu.h:
+
+alt_ucosii_simple_error_check.h:
---
+++ DE2_System/Software/lab_08_2/obj/default/gps.d
@@ -0,0 +1,5 @@
+obj/default/gps.o: gps.c gps.h main.h
+
+gps.h:
+
+main.h:
