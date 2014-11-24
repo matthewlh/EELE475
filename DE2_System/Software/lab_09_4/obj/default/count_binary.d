7f551a446b3f725ec567c86458232a495053d91d
davidkeltgen <dkeltgen45@gmail.com>
2014-11-24 16:39:05 -0700

    Almost finished with lab 9, restarting comp to make eclipse happy

---
+++ DE2_System/Software/lab_09_4/obj/default/count_binary.d
@@ -0,0 +1,34 @@
+obj/default/count_binary.o: count_binary.c count_binary.h \
+  ../lab_09_4_bsp//HAL/inc/alt_types.h \
+  ../lab_09_4_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  ../lab_09_4_bsp//HAL/inc/io.h ../lab_09_4_bsp//HAL/inc/alt_types.h \
+  ../lab_09_4_bsp//drivers/inc/altera_avalon_uart_regs.h \
+  ../lab_09_4_bsp//HAL/inc/sys/alt_irq.h ../lab_09_4_bsp//HAL/inc/nios2.h \
+  ../lab_09_4_bsp//HAL/inc/alt_types.h ../lab_09_4_bsp/system.h \
+  ../lab_09_4_bsp/linker.h ../lab_09_4_bsp/system.h gps.h
+
+count_binary.h:
+
+../lab_09_4_bsp//HAL/inc/alt_types.h:
+
+../lab_09_4_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../lab_09_4_bsp//HAL/inc/io.h:
+
+../lab_09_4_bsp//HAL/inc/alt_types.h:
+
+../lab_09_4_bsp//drivers/inc/altera_avalon_uart_regs.h:
+
+../lab_09_4_bsp//HAL/inc/sys/alt_irq.h:
+
+../lab_09_4_bsp//HAL/inc/nios2.h:
+
+../lab_09_4_bsp//HAL/inc/alt_types.h:
+
+../lab_09_4_bsp/system.h:
+
+../lab_09_4_bsp/linker.h:
+
+../lab_09_4_bsp/system.h:
+
+gps.h:
